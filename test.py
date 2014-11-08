a884316a49b49f7a4e05d86894622efda3804b64
aalitaiga <Adrien@mbp-de-bouz.fritz.box>
2014-11-08 19:07:29 +0100

    implemented tail_log + tests

--- test.py
+++ test.py
@@ -1,5 +1,44 @@
 import unittest
+import os
+from utils import WriteRandomStuff, TailLogFile
 
 
-class TestUtils(unittest.Testcase):
-	
+class TestUtils(unittest.TestCase):
+
+	def test_write_random_stuff(self):
+		if os.path.isfile("test.log"):
+			os.remove("test.log")
+		write = WriteRandomStuff("test.log", 1)
+		write.start()
+		write.join()
+
+		with open("test.log") as f:
+			line = f.readline()
+			self.assertIsNotNone(line)
+
+	def test_tail_log_file(self):
+		if os.path.isfile("test.log"):
+			os.remove("test.log")
+		if os.path.isfile("tail.log"):
+			os.remove("tail.log")
+		write = WriteRandomStuff("test.log", 1)
+		tail = TailLogFile("tail.log")
+		write.start()
+		tail.start()
+		write.join()
+		tail.stop()
+
+		with open("tail.log", 'r') as tail, open("test.log", 'r') as test:
+			tail_lines = tail.readlines()
+			test_lines = test.readlines()
+
+			for tail_line, test_line in zip(tail_lines, test_lines):
+				self.assertEqual(tail_line, test_line)
+
+
+
+
+
+
+if __name__ == '__main__':
+	unittest.main()
--- utils.py
+++ utils.py
@@ -6,19 +6,23 @@ from threading import Thread
 
 class TailLogFile(Thread):
     """ Thread to keep track of the changes in the log file """
-    def __init__(self, file_name):
+    def __init__(self, file_name, refresh_interval=0.1, log_name="tail.log"):
         Thread.__init__(self)
         if not isinstance(file_name, str):
             raise TypeError("Please enter a correct name for the file")
         self.file_name = file_name
+        self.refresh_interval = refresh_interval
+        self.log_name = log_name
+        self.terminated = False
 
-    def run(self, refresh_interval=0.1):     
+    def run(self):     
         if not 'df' in globals():
             initiate_dataframe()
+        print_log = make_a_log_file(self.log_name, to_terminal=False)
         with open(self.file_name, 'r') as file_:
                 # Go to the end of file
                 file_.seek(0,2)
-                while True:
+                while not self.terminated:
                     curr_position = file_.tell()
                     lines = file_.readlines()
                     if not lines:
@@ -26,11 +30,31 @@ class TailLogFile(Thread):
                     else:
                         for line in lines:
                             if line is not None:
-                                print line,
-                    time.sleep(0.1)
+                                print_log.info(line)
+                    time.sleep(self.refresh_interval)
+    def stop(self):
+        self.terminated = True
 
+class WriteRandomStuff(Thread):
+    """ Thread that writes in the time in a log,
+    used for testing. """
 
-def make_a_log_file(name, to_terminal = True, to_filename = True, terminal_level="INFO", file_level = "INFO"):
+    def __init__(self, log_name, duration, time_interval=0.1):
+        Thread.__init__(self)
+        self.duration = duration
+        self.log_name = log_name
+        self.time_interval = time_interval
+
+    def run(self):
+        test_log = make_a_log_file(self.log_name, to_terminal=False)
+        start_time = time.time()
+
+        while time.time() - start_time < self.duration:
+            test_log.info("")
+            time.sleep(self.time_interval)
+
+def make_a_log_file(name, to_terminal = True, to_filename = True,
+                    terminal_level="INFO", file_level = "INFO"):
     """
     Create a new logger or also get the reference from an existing one
     :param name: the name of the logger
@@ -68,7 +92,5 @@ def parse_log(line):
     pass
 
 
-if __name__ == '__main__':
-    log = make_a_log_file("http_monitoring.log")
 
     
