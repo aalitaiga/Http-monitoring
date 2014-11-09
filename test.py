716ab0838c06549dea11f4afd581f305e9ccc956
aalitaiga <Adrien@mbp-de-bouz.fritz.box>
2014-11-10 00:19:50 +0100

    updated regex

--- test.py
+++ test.py
@@ -59,7 +59,7 @@ class TestUtils(unittest.TestCase):
 		test_string4 = '172.16.0.3 - - [25/Sep/2002:14:04:19 +0200]\
 		 "GET / HTTP/1.1" 401 - "" "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.1) Gecko/20020827'
 
-		regex = r'([(\d\.)]+) ([^ ]+) ([^ ]+) \[(.*?)\] "(.*?)" (\d+|-) (\d+|-)( "(.*?)" "(.*?)")'
+		regex = r'([(\d\.)]+) ([^ ]+) ([^ ]+) \[(.*?)\] "(.*?)" (\d+|-) (\d+|-) (?:"(.*?)" "(.*?)")'
 
 		print re.match(regex, test_string4).groups()
 
