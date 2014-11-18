7cbd68cd8c03f54fadb56586e086f9362df37ec4
aalitaiga <adrien.alitaiga@gmail.com>
2014-11-18 19:01:11 +0100

    corrected bugs

--- http_monitoring.py
+++ http_monitoring.py
@@ -2,14 +2,11 @@
 Http Monitoring
 
 Usage:
-    http_monitoring.py
+    http_monitoring.py log_file
 
 Options:
     -h --help       Show this screen.
     log_file        Name or path to the log file to tail.
-    threshold       Number of connection autorized before getting a warning
-    --log           Name of the log file for the output (necessary ??)
-    --mode [PRODUCTION] ou [TEST]
 """
 
 from threading import Thread, RLock
@@ -19,21 +16,6 @@ import pandas as pd
 import re
 import datetime as dt
 
-
-# log = logging.getLogger('http_monitoring')
-# log.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-# fh = logging.FileHandler('http_monitoring.log', encoding='UTF-8')
-# fh.setLevel(logging.INFO)
-# fh.setFormatter(formatter)
-# log.addHandler(fh)
-
-# ch = logging.StreamHandler()
-# ch.setLevel(logging.INFO)
-# ch.setFormatter(formatter)
-# log.addHandler(ch)
-
 # A lock is used to prevent the different threads to access the dataframe
 # at the same time
 lock = RLock()
@@ -42,7 +24,7 @@ T_REPORT = 10
 # Time range window to use to monitor the traffic
 TM_WINDOW = 120
 # Number of connection autorized during the window
-THRESHOLD = 100
+THRESHOLD = 20
 # Global variable to store the data 
 global df
 df = pd.DataFrame(columns=['ip_adress', 'user_id', 'http_code', 'url', 'section', 'time']) 
@@ -80,9 +62,10 @@ class WriteApacheLog(Thread):
         start_time = time.time()
         with open(self.file_name, 'w+') as f:
             while time.time() - start_time < self.duration:
-                string = '127.0.0.1 - adrien [{} -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)" \n'
-                time_now = dt.datetime.strftime(dt.datetime.now(),"%d/%b/%Y:%H:%M:%S")
-                f.write(string.format(time_now))
+                with lock:
+                    string = '127.0.0.1 - adrien [{} -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)" \n'
+                    time_now = dt.datetime.strftime(dt.datetime.now(),"%d/%b/%Y:%H:%M:%S")
+                    f.write(string.format(time_now))
                 time.sleep(self.time_interval)
 
 class TailLogFile(Thread):
@@ -129,9 +112,8 @@ def add_to_df(line):
     items = [parsed_line[i] for i in [0, 2, 5, 7]]
     section = '/'.join(items[3].split('/')[:4])
     items.extend([section, time])
-    with lock:
-        df.loc[len(df)+1] = items
-        df.replace('-', pd.np.nan, inplace=True)
+    df.loc[len(df)+1] = items
+    df.replace('-', pd.np.nan, inplace=True)
 
 class SendReport(Thread):
     """ Thread used to send a report every 10s """
@@ -268,6 +250,10 @@ if __name__ == '__main__':
     args = docopt(__doc__, version='Http Monitoring 1.0')
 
     log = make_a_log_file('http_monitoring')
+
+    write = WriteApacheLog('test.log', 100)
+    write.start()
+
     tail = TailLogFile('test.log')
     tail.start()
 
@@ -277,6 +263,21 @@ if __name__ == '__main__':
     report = SendReport()
     report.start()
 
+    # args = docopt(__doc__, version='Http Monitoring 1.0')
+
+    # log = make_a_log_file('http_monitoring')
+
+    # write = WriteApacheLog('test.log', 100)
+    # write.start()
+
+    # tail = TailLogFile(args['log_file'])
+    # tail.start()
+
+    # monitor = MonitorTraffic()
+    # monitor.start()
+
+    # report = SendReport()
+    # report.start()
 
 
 
